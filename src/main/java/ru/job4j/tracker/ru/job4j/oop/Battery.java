d98aa711d506f00818d58bd4680af75e0bc3574f
aswsx <61579000+aswsx@users.noreply.github.com>
2020-08-28 11:00:59 +0600

    1.7. Состояние объекта [#343807]

--- src/main/java/ru/job4j/tracker/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/tracker/ru/job4j/oop/Battery.java
@@ -2,22 +2,22 @@ package ru.job4j.tracker.ru.job4j.oop;
 
 public class Battery {
 
-    private int level;
+    private int load;
 
-    public Battery(int level) {
-        this.level = level;
+    public Battery(int load) {
+        this.load = load;
     }
 
-    public void pour(Battery another) {
-        this.level = this.level + another.level;
-        another.level = 0;
+    public void exchange(Battery another) {
+        another.load = this.load + another.load;
+        this.load = 0;
     }
 
     public static void main(String[] args) {
         Battery first = new Battery(80);
         Battery second = new Battery(15);
-        System.out.println("first : " + first.level + ". second : " + second.level);
-        first.pour(second);
-        System.out.println("first : " + first.level + ". second : " + second.level);
+        System.out.println("first : " + first.load + ". second : " + second.load);
+        first.exchange(second);
+        System.out.println("first : " + first.load + ". second : " + second.load);
     }
 }
