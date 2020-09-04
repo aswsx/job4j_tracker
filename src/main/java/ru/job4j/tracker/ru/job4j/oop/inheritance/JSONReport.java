f733be88e9b2fb9ed7bff35ca8b56c66e2c58db0
aswsx <61579000+aswsx@users.noreply.github.com>
2020-09-04 12:56:54 +0600

    5. Аннотация @Override [#343833]

--- src/main/java/ru/job4j/tracker/ru/job4j/oop/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/tracker/ru/job4j/oop/inheritance/JSONReport.java
@@ -1,12 +1,9 @@
 package ru.job4j.tracker.ru.job4j.oop.inheritance;
 
 public class JSONReport extends TextReport{
+    @Override
     public String generate(String name, String body) {
         return "\"name\" " + ": " + "\"" + name + "\"" + System.lineSeparator() + "\"body\" " +
                 ": " + "\"" + body + "\"";
     }
 }
-//{
-//  "name" : "name",
-//  "body" : "body"
-//}
