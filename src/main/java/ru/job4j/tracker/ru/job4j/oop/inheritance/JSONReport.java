dd030254a64f73d0878b30b0deae7eb66c86f114
aswsx <61579000+aswsx@users.noreply.github.com>
2020-09-04 13:40:35 +0600

    4. Переопределение [#343832]

--- src/main/java/ru/job4j/tracker/ru/job4j/oop/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/tracker/ru/job4j/oop/inheritance/JSONReport.java
@@ -1,9 +1,10 @@
 package ru.job4j.tracker.ru.job4j.oop.inheritance;
 
-public class JSONReport extends TextReport{
+public class JSONReport extends TextReport {
     @Override
     public String generate(String name, String body) {
-        return "\"name\" " + ": " + "\"" + name + "\"" + System.lineSeparator() + "\"body\" " +
-                ": " + "\"" + body + "\"";
+        return "{" + System.lineSeparator() + "\"name\" " + ": " + "\"" + name + "\"" + ','
+                + System.lineSeparator() + "\"body\" " + ": " + "\"" + body + "\""
+                + System.lineSeparator() + "}";
     }
 }
