177276d7d0983ceeb1600dbf38730e643367b69e
aswsx <61579000+aswsx@users.noreply.github.com>
2020-09-04 13:35:33 +0600

    3. Реализация профессий в коде [#343837]

---
+++ src/main/java/ru/job4j/tracker/ru/job4j/oop/Builder.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker.ru.job4j.oop;
+
+public class Builder extends Engineer {
+    private int experience;
+    public String speciality;
+
+    public void construction() {
+    }
+
+    public void finishing() {
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/ru/job4j/oop/Programmer.java
@@ -0,0 +1,15 @@
+package ru.job4j.tracker.ru.job4j.oop;
+
+public class Programmer extends Engineer {
+    private int experience;
+    public String language;
+
+    public void algorithmDevelopment() {
+    }
+
+    public void coding() {
+    }
+
+    public void debugging() {
+    }
+}
