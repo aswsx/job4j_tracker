b0f3f7611835c1b5bd98f7783f1baae76553b2e0
Alex <lextor78@gmail.com>
2020-09-09 20:36:01 +0600

    2. Рефакторинг - Шаблон Декоратор для валидатора. [#343783]

---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class ValidateInputTest {
+
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[]{"one", "1"});
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenValidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[]{"1"});
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenOutput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[]{"0", "1"});
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {new ExitAction(out)};
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is("Menu." + System.lineSeparator() + "0. Exit" + System.lineSeparator()));
+    }
+}
