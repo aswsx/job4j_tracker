ae733720ec3ee9041e08fdff4c691a6d047134fb
aswsx <61579000+aswsx@users.noreply.github.com>
2020-09-17 18:51:10 +0600

    test

--- src/main/java/ru/job4j/tracker/ru/job4j/oop/inheritance/HtmlReport.java
+++ src/main/java/ru/job4j/tracker/ru/job4j/oop/inheritance/HtmlReport.java
@@ -5,4 +5,4 @@ public class HtmlReport extends TextReport {
     public String generate(String name, String body) {
         return "<h1>" + name + "</h1>" + "<br/>" + "<span>" + body + "</span>";
     }
-}
+}vv
