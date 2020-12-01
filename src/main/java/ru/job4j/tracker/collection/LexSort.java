2971be1c27110ce3105a83607ac3e09427f59f54
aswsx <61579000+aswsx@users.noreply.github.com>
2020-12-01 16:38:53 +0600

    Correction

--- src/main/java/ru/job4j/tracker/collection/LexSort.java
+++ src/main/java/ru/job4j/tracker/collection/LexSort.java
@@ -6,6 +6,8 @@ public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        return Integer.compare(Integer.parseInt(left.split("[.]")[0]), Integer.parseInt(right.split("[.]")[0]));
+        int leftComp = Integer.parseInt(left.split("[.]")[0]);
+        int rightComp = Integer.parseInt(right.split("[.]")[0]);
+        return Integer.compare(leftComp, rightComp);
     }
 }
