39dd0803e87eb2fc9362f50173e49659f00abc69
aswsx <61579000+aswsx@users.noreply.github.com>
2020-12-03 17:36:00 +0600

    1. Анонимные классы [#343573]

---
+++ src/main/java/ru/job4j/lambda/SearchAtt.java
@@ -0,0 +1,29 @@
+package ru.job4j.lambda;
+
+import ru.job4j.lambda.Attachment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchAtt {
+
+    public static List<Attachment> filterSize(List<Attachment> list) {
+        List<Attachment> rsl = new ArrayList<>();
+        for (Attachment att : list) {
+            if (att.getSize() > 100) {
+                rsl.add(att);
+            }
+        }
+        return rsl;
+    }
+
+    public static List<Attachment> filterName(List<Attachment> list) {
+        List<Attachment> rsl = new ArrayList<>();
+        for (Attachment att : list) {
+            if (att.getName().contains("bug")) {
+                rsl.add(att);
+            }
+        }
+        return rsl;
+    }
+}
