f6146869db338b2d467801070127f5e8e3ac02a3
aswsx <61579000+aswsx@users.noreply.github.com>
2020-12-06 14:21:24 +0600

    5. Изменение уже существующей записи в отображении.

---
+++ src/main/java/ru/job4j/map/ComputeIfPresent.java
@@ -0,0 +1,13 @@
+package ru.job4j.map;
+
+import java.util.Map;
+
+public class ComputeIfPresent {
+    public static Map<Integer, String> collectData(
+            Map<Integer, String> name, Map<Integer, String> surname) {
+        for(int ind : name.keySet()) {
+            name.computeIfPresent(ind, (key, value) -> value + " " + surname.get(ind));
+        }
+        return name;
+    }
+}
