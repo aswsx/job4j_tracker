cab9dcfea46d81d5c90eeaa1398927473a4c9491
aswsx <61579000+aswsx@users.noreply.github.com>
2020-12-06 13:24:39 +0600

    13. Данные об осадках

---
+++ src/main/java/ru/job4j/map/Weather.java
@@ -0,0 +1,55 @@
+package ru.job4j.map;
+
+import java.util.*;
+
+public class Weather {
+    public static List<Info> editData(List<Info> list) {
+        List<Info> rsl = new ArrayList<>();
+        Map<String, Integer> city = new HashMap<>();
+        for(Info elem : list) {
+            city.computeIfPresent(elem.getCity(), (key, val) -> val + elem.getRainfall());
+            city.putIfAbsent(elem.getCity(), elem.getRainfall());
+        }
+        for (String str : city.keySet()) {
+            rsl.add(new Info(str, city.get(str)));
+        }
+        return rsl;
+    }
+
+    public static class Info {
+        private String city;
+
+        private int rainfall;
+
+        public Info(String city, int rainfall) {
+            this.city = city;
+            this.rainfall = rainfall;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public int getRainfall() {
+            return rainfall;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Info info = (Info) o;
+            return rainfall == info.rainfall &&
+                    Objects.equals(city, info.city);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(city, rainfall);
+        }
+    }
+}
