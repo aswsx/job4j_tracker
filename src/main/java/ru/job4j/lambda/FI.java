6ac19a7263f27f415a4d36897c31403727321dee
aswsx <61579000+aswsx@users.noreply.github.com>
2020-12-20 00:25:33 +0600

    2.2. Лямбда блок [#343575]

---
+++ src/main/java/ru/job4j/lambda/FI.java
@@ -0,0 +1,30 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class FI {
+    public static void main(String[] args) {
+        Attachment[] atts = {
+                new Attachment("image 1", 20),
+                new Attachment("image 3", 120),
+                new Attachment("image 2", 23)
+        };
+        Comparator<Attachment> cmpText = (left, right) -> {
+            System.out.println("compare text - " + left.getName() + " : " + right.getName());
+            return left.getName().compareTo(right.getName());
+        };
+
+        Comparator<Attachment> cmpDescSize = (left, right) -> {
+            System.out.println("compare length desc - " + left.getName().length() + " : " + right.getName().length());
+            return Integer.compare(right.getName().length(), left.getName().length());
+        };
+        Comparator<Attachment> comparator = (left, right) -> {
+            System.out.println("compare size asc- " + left.getSize() + " : " + right.getSize());
+            return left.getSize() - right.getSize();
+        };
+        Arrays.sort(atts, comparator);
+        Arrays.sort(atts, cmpText);
+        Arrays.sort(atts,cmpDescSize);
+    }
+}
