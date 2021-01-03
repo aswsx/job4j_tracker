8fba991ce629c020cf17880a66147e6b5ee57848
aswsx <61579000+aswsx@users.noreply.github.com>
2021-01-03 19:21:09 +0600

    3. Уникальность элементов и сортировка. [#343585]

--- src/main/java/ru/job4j/stream/Address.java
+++ src/main/java/ru/job4j/stream/Address.java
@@ -4,31 +4,13 @@ import java.util.Objects;
 
 public class Address {
     private final String city;
-    private final String street;
-    private final int home;
-    private final int apartment;
 
-    public Address(String city, String street, int home, int apartment) {
+    public Address(String city) {
         this.city = city;
-        this.street = street;
-        this.home = home;
-        this.apartment = apartment;
     }
 
     public String getCity() {
         return city;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public int getHome() {
-        return home;
-    }
-
-    public int getApartment() {
-        return apartment;
     }
 
     @Override
@@ -40,12 +22,11 @@ public class Address {
             return false;
         }
         Address address = (Address) o;
-        return city.equals(address.getCity()) && street.equals(address.getStreet())
-                && home == address.getHome() && apartment == address.getApartment();
+        return city.equals(address.getCity());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(city, street, home, apartment);
+        return Objects.hash(city);
     }
 }
--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -10,17 +10,38 @@ import static org.junit.Assert.assertThat;
 
 public class ProfilesTest {
     @Test
-    public void collect() {
+    public void whenDifferentAddresses() {
         Profiles func = new Profiles();
         List<Address> expect = List.of(
-                new Address("Нур-Султан", "Мира", 23, 12),
-                new Address("Алматы", "Нурмакова", 30, 3),
-                new Address("Караганда", "Ленина", 21, 14),
-                new Address("Москва", "ак. Королева", 12, 1)
+                new Address("Алматы"),
+                new Address("Караганда"),
+                new Address("Москва"),
+                new Address("Нур-Султан")
         );
         List<Profile> input = expect.stream()
                 .map(Profile::new)
                 .collect(Collectors.toList());
+        List<Address> rsl = func.collect(input);
+        assertThat(rsl, is(expect));
+    }
+
+    @Test
+    public void whenSameAddresses() {
+        Profiles func = new Profiles();
+        List<Address> list = List.of(
+                new Address("Алматы"),
+                new Address("Алматы"),
+                new Address("Москва"),
+                new Address("Нур-Султан")
+        );
+        List<Profile> input = list.stream()
+                .map(Profile::new)
+                .collect(Collectors.toList());
+        List<Address> expect = List.of(
+                new Address("Алматы"),
+                new Address("Москва"),
+                new Address("Нур-Султан")
+        );
         List<Address> rsl = func.collect(input);
         assertThat(rsl, is(expect));
     }
