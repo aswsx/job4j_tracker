1440563a068bafceb554adc9c1fc5242e170a1d2
aswsx <61579000+aswsx@users.noreply.github.com>
2021-01-03 19:23:37 +0600

    3. Уникальность элементов и сортировка. [#343585]

--- src/main/java/ru/job4j/stream/Address.java
+++ src/main/java/ru/job4j/stream/Address.java
@@ -4,9 +4,15 @@ import java.util.Objects;
 
 public class Address {
     private final String city;
+    private final String street;
+    private final int home;
+    private final int apartment;
 
-    public Address(String city) {
+    public Address(String city, String street, int home, int apartment) {
         this.city = city;
+        this.street = street;
+        this.home = home;
+        this.apartment = apartment;
     }
 
     public String getCity() {
--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -13,10 +13,10 @@ public class ProfilesTest {
     public void whenDifferentAddresses() {
         Profiles func = new Profiles();
         List<Address> expect = List.of(
-                new Address("Алматы"),
-                new Address("Караганда"),
-                new Address("Москва"),
-                new Address("Нур-Султан")
+                new Address("Алматы", "Нурмакова", 30, 3),
+                new Address("Караганда", "Ленина", 21, 14),
+                new Address("Москва", "ак. Королева", 12, 1),
+                new Address("Нур-Султан", "Мира", 23, 12)
         );
         List<Profile> input = expect.stream()
                 .map(Profile::new)
@@ -29,18 +29,18 @@ public class ProfilesTest {
     public void whenSameAddresses() {
         Profiles func = new Profiles();
         List<Address> list = List.of(
-                new Address("Алматы"),
-                new Address("Алматы"),
-                new Address("Москва"),
-                new Address("Нур-Султан")
+                new Address("Алматы", "Нурмакова", 30, 3),
+                new Address("Алматы", "Нурмакова", 30, 3),
+                new Address("Москва", "ак. Королева", 12, 1),
+                new Address("Нур-Султан", "Мира", 23, 12)
         );
         List<Profile> input = list.stream()
                 .map(Profile::new)
                 .collect(Collectors.toList());
         List<Address> expect = List.of(
-                new Address("Алматы"),
-                new Address("Москва"),
-                new Address("Нур-Султан")
+                new Address("Алматы", "Нурмакова", 30, 3),
+                new Address("Москва", "ак. Королева", 12, 1),
+                new Address("Нур-Султан", "Мира", 23, 12)
         );
         List<Address> rsl = func.collect(input);
         assertThat(rsl, is(expect));
