144a92860807a2ce862eb09de32cc86cfe1c3e4c
aswsx <61579000+aswsx@users.noreply.github.com>
2021-01-03 20:32:45 +0600

    4. Преобразование List в Map. [#343586]

---
+++ src/test/java/ru/job4j/stream/listToMapTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class listToMapTest {
+    @Test
+    public void convert() {
+        List<Student> input = List.of(
+                new Student(1, "Челентано"),
+                new Student(2, "Делон"),
+                new Student(3, "Депардье"),
+                new Student(4, "Ришар")
+        );
+        Map<String, Student> rsl = ListToMap.convert(input);
+        Map expect = Map.of(
+                "Челентано", new Student(1, "Челентано"),
+                "Делон", new Student(2, "Делон"),
+                "Депардье", new Student(3, "Депардье"),
+                "Ришар", new Student(4, "Ришар")
+        );
+        assertThat(rsl, is(expect));
+    }
+}
