413a85af5a5d5eb969a974a071144382b33576c5
Alex <lextor78@gmail.com>
2021-01-10 12:50:06 +0600

    4.4. Method reference и Supplier

---
+++ src/main/java/ru/job4j/lambda/MRSupplier.java
@@ -0,0 +1,10 @@
+package ru.job4j.lambda;
+
+import java.util.function.Supplier;
+
+public class MRSupplier {
+
+    public static Supplier<String> supplier() {
+        return String::new;
+    }
+}
