3b9ddf2df73d4088571fde41f5e124abf198d715
Alex <lextor78@gmail.com>
2021-01-10 21:55:27 +0600

    5.1. Промежуточные операции. Метод map(). Map с числами

---
+++ src/main/java/ru/job4j/stream/NumberMap.java
@@ -0,0 +1,12 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class NumberMap {
+    public static List<Integer> map(List<Integer> numbers) {
+        return numbers.stream()
+                .map(n -> n * n)
+                .collect(Collectors.toList());
+    }
+}
