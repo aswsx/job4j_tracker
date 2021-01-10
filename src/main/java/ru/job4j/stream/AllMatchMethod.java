9428f13e8b8a38c0aeeb8f0dee8c004997258aca
Alex <lextor78@gmail.com>
2021-01-10 21:29:23 +0600

    4.2. Терминальные операции. Метод allMatch()

---
+++ src/main/java/ru/job4j/stream/AllMatchMethod.java
@@ -0,0 +1,10 @@
+package ru.job4j.stream;
+
+import java.util.List;
+
+public class AllMatchMethod {
+    public static boolean check(List<String> list) {
+        return list.stream()
+                .allMatch(n -> n.length() > 3);
+    }
+}
