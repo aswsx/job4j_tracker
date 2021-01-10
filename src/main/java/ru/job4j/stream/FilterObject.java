0eb1f37de07f1d86f53cc6cdbfeb73beb24269e3
Alex <lextor78@gmail.com>
2021-01-10 22:12:54 +0600

    6.3. Промежуточные операции. Метод filter(). Фильтрация объектов

---
+++ src/main/java/ru/job4j/stream/FilterObject.java
@@ -0,0 +1,34 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FilterObject {
+
+    public static class User {
+
+        private String name;
+
+        private int age;
+
+        public User(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+    }
+
+    public static List<User> filter(List<User> users) {
+        return users.stream()
+                .filter(s -> s.getName().startsWith("B"))
+                .filter(n -> n.getAge() > 18)
+                .collect(Collectors.toList());
+    }
+}
