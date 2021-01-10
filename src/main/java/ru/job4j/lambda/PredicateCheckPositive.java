84a81f75afbbe617c79a06be788244839b6cc1dc
aswsx <61579000+aswsx@users.noreply.github.com>
2021-01-10 12:10:05 +0600

    1.1. Predicate. Проверить, что число положительное

---
+++ src/main/java/ru/job4j/lambda/PredicateCheckPositive.java
@@ -0,0 +1,14 @@
+package ru.job4j.lambda;
+
+import java.util.function.Predicate;
+
+public class PredicateCheckPositive {
+
+    public static boolean check(int num) {
+        return check(a -> a > 0, num);
+    }
+
+    private static boolean check(Predicate<Integer> predicate, int num) {
+        return predicate.test(num);
+    }
+}
