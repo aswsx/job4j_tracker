4ed6354dd6e72efed4ec8742f086d431d9f0d858
Tosh-PC <lextor78@gmail.com>
2021-03-22 19:50:33 +0600

    corr

--- src/main/java/ru/job4j/lambda/OptionalOfAndEmpty.java
+++ src/main/java/ru/job4j/lambda/OptionalOfAndEmpty.java
@@ -5,10 +5,8 @@ import java.util.Optional;
 
 public class OptionalOfAndEmpty {
     public static Optional<String> findValue(List<String> strings, String value) {
-        for (String string : strings) {
-            if (string.equals(value)) {
-                return Optional.of(value);
-            }
+        if (strings.contains(value)) {
+            return Optional.of(value);
         }
         return Optional.empty();
     }
