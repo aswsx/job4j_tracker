01dba3392e040521b8800b928890672452c2026f
Tosh-PC <lextor78@gmail.com>
2021-03-24 00:14:42 +0600

    3. Коллекции. Доп. задача

--- src/main/java/ru/job4j/collection/Article.java
+++ src/main/java/ru/job4j/collection/Article.java
@@ -1,13 +1,15 @@
 package ru.job4j.collection;
 
-import java.util.List;
+import java.util.Arrays;
+import java.util.HashSet;
 
 public class Article {
     public static boolean generateBy(String origin, String line) {
-        List<String> sourceText = List.of(origin.split("\\s*(\\s|,|!|\\.)\\s*"));
-        List<String> outText = List.of(line.split("\\s*(\\s|,|!|\\.)\\s*"));
+        HashSet<String> sourceText = new HashSet<>(Arrays
+                .asList(origin.split("\\s*(\\s|,|!|\\.)\\s*")));
+        HashSet<String> outText = new HashSet<>(Arrays.asList(line.split("\\s*(\\s|,|!|\\.)\\s*")));
         for (String str : outText) {
-            if (!sourceText.contains(str)) {
+            if (!sourceText.toString().contains(str)) {
                 return false;
             }
         }
