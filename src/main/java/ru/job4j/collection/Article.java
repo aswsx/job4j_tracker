b901736b75a3f67952d192c1a3b7f5cc640b7171
Tosh-PC <lextor78@gmail.com>
2021-03-24 11:01:39 +0600

    3. Коллекции. Доп. задача

--- src/main/java/ru/job4j/collection/Article.java
+++ src/main/java/ru/job4j/collection/Article.java
@@ -6,11 +6,15 @@ import java.util.HashSet;
 public class Article {
     public static boolean generateBy(String origin, String line) {
         HashSet<String> sourceText = new HashSet<>(Arrays
-                .asList(origin.split("\\s*(\\s|,|!|\\.)\\s*")));
+                .asList(origin.
+                        replaceAll("\\p{P}", "").
+                        split("\\p{Z}")));
         HashSet<String> outText = new HashSet<>(Arrays
-                .asList(line.split("\\s*(\\s|,|!|\\.)\\s*")));
+                .asList(line.
+                        replaceAll("\\p{P}", "").
+                        split("\\p{Z}")));
         for (String str : outText) {
-            if (!sourceText.toString().contains(str)) {
+            if (!sourceText.contains(str)) {
                 return false;
             }
         }
