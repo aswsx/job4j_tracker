65cfd3d7c30706fcc25b4fbb06d14b6c75854161
Tosh-PC <lextor78@gmail.com>
2021-03-27 14:24:53 +0600

    1. Экзамен. Lambda, Stream API, JDK Features доп.задача

--- src/main/java/ru/job4j/stream/EasyStream.java
+++ src/main/java/ru/job4j/stream/EasyStream.java
@@ -1,52 +1,63 @@
 package ru.job4j.stream;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class EasyStream {
-    private List<Integer> rsl; //TODO выходной массив, возвращаемый методом collect
-    //TODO для создания потока нужен итератор?
+    private List<Integer> rsl;
 
-    //TODO метод принимает входной массив и ... Что и в каком виде он возвращает?
+    public EasyStream(List<Integer> rsl) {
+        this.rsl = rsl;
+    }
+
     public static EasyStream of(List<Integer> source) {
-        throw new UnsupportedOperationException();
+        return new EasyStream(source);
     }
-    //TODO метод принимает элементы(из потока?) и применяет к ним функцию.
-    // Как функция передается в метод? Как передать сюда поток?
+
     public EasyStream map(Function<Integer, Integer> fun) {
-        throw new UnsupportedOperationException();
+        List<Integer> temp = new ArrayList<>();
+        for (int i = 0; i < rsl.size(); i++) {
+            temp.add(i, fun.apply(rsl.get(i)));
+        }
+        return new EasyStream(temp);
     }
-    //TODO метод принимает элементы (из потока?) и применяет фильтр
+
     public EasyStream filter(Predicate<Integer> fun) {
-        throw new UnsupportedOperationException();
+        List<Integer> temp = new ArrayList<>();
+        for (int i = 0; i < rsl.size(); i++) {
+            if (fun.test(rsl.get(i))) {
+                temp.add(rsl.get(i));
+            }
+        }
+        return new EasyStream(temp);
     }
-    //TODO собрать элементы (из потока?) в массив(коллекцию) rsl
+
     public List<Integer> collect() {
         return rsl;
     }
 
-    //TODO Билдер собирает в кучу методы, нужные для создания потока? Шаблон переделать
-    public static class Builder {
-        private EasyStream eStream; //создаем переменную класса EasyStream
-
-        private Builder() {
-            eStream = new EasyStream(); //конструктор создает объект EasyStream
-        }
-
-        public Builder listToStream(List<Integer> source) { //??? пройти итератором по
-            // принимаемому на вход List<Integer>? iterator или listIterator? Что сделать с
-            //полученными элементами?
-            source.listIterator();
-            return this;
-        }
-
-        public Builder res() { //??? что дальше? вернуть какой-то результат? какой?
-            return this;
-        }
-
-        public EasyStream build() { //метод возвращает собранное лего
-            return eStream;
-        }
-    }
+//    public static class Builder {
+//        private EasyStream eStream; //создаем переменную класса EasyStream
+//
+//        private Builder() {
+//            eStream = new EasyStream(source); //конструктор создает объект EasyStream
+//        }
+//
+//        public Builder listToStream(List<Integer> source) { //??? пройти итератором по
+//            // принимаемому на вход List<Integer>? iterator или listIterator? Что сделать с
+//            //полученными элементами?
+//            ListIterator<Integer> literator = source.listIterator();
+//            return this;
+//        }
+//
+//        public Builder res() { //??? что дальше? вернуть какой-то результат? какой?
+//            return this;
+//        }
+//
+//        public EasyStream build() { //метод возвращает собранное лего
+//            return eStream;
+//        }
+    //}
 }
