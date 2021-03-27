60a7442a9c65ade0e43c4b9fb1a9389e40d49bb7
Tosh-PC <lextor78@gmail.com>
2021-03-27 21:00:27 +0600

    1. Экзамен. Lambda, Stream API, JDK Features доп.задача

--- src/main/java/ru/job4j/stream/EasyStream.java
+++ src/main/java/ru/job4j/stream/EasyStream.java
@@ -6,22 +6,43 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class EasyStream {
-    private final List<Integer> rsl;
-    private final List<Integer> temp = new ArrayList<>();
+    private List<Integer> rsl;
+    private List<Integer> temp;
 
-    private EasyStream(List<Integer> rsl) {
-        this.rsl = rsl;
+    static class Builder {
+        private List<Integer> rsl;
+        private List<Integer> temp;
+
+        Builder buildRsl(List<Integer> rsl) {
+            this.rsl = rsl;
+            return this;
+        }
+
+        Builder buildTemp() {
+            this.temp = new ArrayList<>();
+            return this;
+        }
+
+        EasyStream build() {
+            EasyStream easyStream = new EasyStream();
+            easyStream.rsl = rsl;
+            easyStream.temp = temp;
+            return easyStream;
+        }
     }
 
     public static EasyStream of(List<Integer> source) {
-        return new EasyStream(source);
+        return new Builder()
+                .buildRsl(source)
+                .buildTemp()
+                .build();
     }
 
     public EasyStream map(Function<Integer, Integer> fun) {
-        for (Integer integer : rsl) {
-            temp.add(fun.apply(integer));
+        for (int elem : rsl) {
+            temp.add(fun.apply(elem));
         }
-        return new EasyStream(temp);
+        return new EasyStream();
     }
 
     public EasyStream filter(Predicate<Integer> fun) {
@@ -30,33 +51,10 @@ public class EasyStream {
                 temp.add(elem);
             }
         }
-        return new EasyStream(temp);
+        return new EasyStream();
     }
 
     public List<Integer> collect() {
         return rsl;
     }
-
-//    public static class Builder {
-//        private EasyStream eStream; //создаем переменную класса EasyStream
-//
-//        private Builder() {
-//            eStream = new EasyStream(source); //конструктор создает объект EasyStream
-//        }
-//
-//        public Builder listToStream(List<Integer> source) { //??? пройти итератором по
-//            // принимаемому на вход List<Integer>? iterator или listIterator? Что сделать с
-//            //полученными элементами?
-//            ListIterator<Integer> literator = source.listIterator();
-//            return this;
-//        }
-//
-//        public Builder res() { //??? что дальше? вернуть какой-то результат? какой?
-//            return this;
-//        }
-//
-//        public EasyStream build() { //метод возвращает собранное лего
-//            return eStream;
-//        }
-    //}
 }
