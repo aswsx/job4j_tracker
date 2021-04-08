bbba3387cbdf590cf73da5b322b4318aa5d3b6b7
Tosh-PC <lextor78@gmail.com>
2021-04-09 01:04:08 +0600

    corr

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,9 +5,7 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        String[] dep1 = o1.split("/");
-        String[] dep2 = o2.split("/");
-        int depComp = dep2[0].compareTo(dep1[0]);
+        int depComp = o2.split("/")[0].compareTo(o1.split("/")[0]);
         if (depComp != 0) {
             return depComp;
         }
