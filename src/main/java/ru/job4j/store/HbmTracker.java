2953c8c9367dc1c975511f44cb78cf4c8dfa3af3
Tosh-PC <lextor78@gmail.com>
2022-03-23 23:10:48 +0600

    1. HbmTracker.

---
+++ src/main/java/ru/job4j/store/HbmTracker.java
@@ -0,0 +1,60 @@
+package ru.job4j.store;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import ru.job4j.model.Item;
+
+import java.util.List;
+
+/**
+ * @author Alex Gutorov
+ * @version 1.0
+ * @created 23/03/2022 - 23:08
+ */
+public class HbmTracker implements Store, AutoCloseable {
+    private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure().build();
+    private final SessionFactory sf = new MetadataSources(registry)
+            .buildMetadata().buildSessionFactory();
+
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public Item add(Item item) {
+        return null;
+    }
+
+    @Override
+    public boolean replace(int id, Item item) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return false;
+    }
+
+    @Override
+    public List<Item> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Item> findByName(String key) {
+        return null;
+    }
+
+    @Override
+    public Item findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void close() throws Exception {
+        StandardServiceRegistryBuilder.destroy(registry);
+    }
+}
